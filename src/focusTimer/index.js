25a34decf56c8d4256370449b2ff74059bb9f605
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-08 22:18:51 +0100

    create necessary events

--- src/focusTimer/index.js
+++ src/focusTimer/index.js
@@ -1,7 +1,10 @@
 import state from "./state";
+import * as events from "./events"
 
 export function start(minutes, seconds){
   state.minutes = minutes
   state.seconds = seconds
+
+  events.registerControls()
 }
 

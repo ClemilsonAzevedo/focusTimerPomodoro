c4d2851becbb43b53816fb7e107ed5a6d977e878
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-15 03:20:38 +0100

    Aplication state

--- src/focusTimer/actions.js
+++ src/focusTimer/actions.js
@@ -1,5 +1,7 @@
+import state from './state.js'
+
 export function toggleRunning() {
-  
+  state.isRunning = document.documentElement.classList.toggle('running')
 }
 
 export function set(){
@@ -7,5 +9,10 @@ export function set(){
 }
 
 export function reset() {
-  
+  state.isRunning = false
+  document.documentElement.classList.remove('running')
+}
+
+export function toggleMusic(){
+  state.isMute = document.documentElement.classList.toggle('music-on')
 }

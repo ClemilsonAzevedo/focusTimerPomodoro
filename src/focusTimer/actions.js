02c8b2a6b88f1d8a32d868b08d0a5d8e4cfd71e6
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-16 02:24:39 +0100

    ...

--- src/focusTimer/actions.js
+++ src/focusTimer/actions.js
@@ -1,5 +1,6 @@
 import state from './state.js'
 import * as timer from './timer.js'
+import * as el from './elements.js'
 
 export function toggleRunning() {
   state.isRunning = document.documentElement.classList.toggle('running')
@@ -7,8 +8,8 @@ export function toggleRunning() {
   timer.countDown()
 }
 
-export function set(){
-  
+export function set() {
+  el.minutes.setAttribute('contenteditable', true)
 }
 
 export function reset() {

b723643c66628f8f095745e9938a89f9514a008c
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-16 02:34:28 +0100

    regular expression validating

--- src/focusTimer/events.js
+++ src/focusTimer/events.js
@@ -1,8 +1,8 @@
-import { controls } from "./elements.js";
+import * as el from "./elements.js";
 import * as actions from './actions.js'
 
 export function registerControls(){
-  controls.addEventListener('click', (event) => {
+  el.controls.addEventListener('click', (event) => {
   const action = event.target.dataset.action
 
   if (typeof actions[action] !== 'function'){
@@ -12,4 +12,13 @@ export function registerControls(){
   actions[action]()
   
   })
+}
+
+export function setMinutes(){
+
+  el.minutes.addEventListener('focus', () =>{
+    el.minutes.textContent = ''
+  })
+// A linha abaixo coloca a verificacao com uma expressao regular para que o campo so receba numeros
+  el.minutes.onkeypress = (event) => /\d/.test(event.key) 
 }

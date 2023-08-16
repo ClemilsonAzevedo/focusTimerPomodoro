2ce21b8f8d5ec05a0d9a436dc50b743c3396c713
Clemilson Azevedo <Clemilsondeazevedo@gmail.com>
2023-08-16 02:42:30 +0100

    finishing minutes choice

--- src/focusTimer/events.js
+++ src/focusTimer/events.js
@@ -1,5 +1,7 @@
 import * as el from "./elements.js";
 import * as actions from './actions.js'
+import { updateDisplay } from "./timer.js";
+import state from "./state.js";
 
 export function registerControls(){
   el.controls.addEventListener('click', (event) => {
@@ -21,4 +23,16 @@ export function setMinutes(){
   })
 // A linha abaixo coloca a verificacao com uma expressao regular para que o campo so receba numeros
   el.minutes.onkeypress = (event) => /\d/.test(event.key) 
+
+  el.minutes.addEventListener('blur', (event) => {
+    let time = event.currentTarget.textContent
+    time = time > 60 ? 60 : time
+
+    state.minutes = time
+    state.seconds = 0
+
+    updateDisplay()
+
+    el.minutes.removeAttribute('contenteditable')
+  })
 }
